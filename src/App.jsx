310e938477d962c33ac0442385b021e50afa7b8f
Zlatina Hristova <zlatina.nyagolova@gmail.com>
2025-10-04 16:35:21 +0300

    Adding cute effect when egg is ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import EggSelector from "./components/EggSelector";
 function App() { 
   const [selectedEgg, setSelectedEgg] = useState(null);
   const [eggBoilTime, setEggBoilTime] = useState(null);
+  const [isReady, setIsReady] = useState(false);
+
 
   const handleSelect = (eggType, eggBoilMin) => {
     console.log("Selected egg:", eggType);
     console.log("Min to boil:", eggBoilMin);
     setSelectedEgg(eggType);
     setEggBoilTime(eggBoilMin);
+    setIsReady(false);
   };
 
   return (
@@ -20,7 +23,21 @@ function App() {
       {!selectedEgg && <EggSelector onSelect={handleSelect} />}
       {selectedEgg && (
         <div className="flex flex-col justify-center items-center h-screen">
-          <img src={`/src/assets/${selectedEgg.toLowerCase()}.svg`} alt={selectedEgg} className="w-50 h-50 mb-4" />
+          <div className="relative w-48 h-48 mb-4">
+            {/* Base egg image */}
+            <img
+              src={`/src/assets/${selectedEgg.toLowerCase()}.svg`}
+              alt={selectedEgg}
+              className="w-full h-full"
+            />
+
+            {/* adding eyes + bubble overlay when ready */}
+          <img
+            src="/src/assets/ready.svg"
+            alt="Ready!"
+            className={`ready-bubble pointer-events-none ${isReady ? "block" : "hidden"}`}
+          />
+          </div>
           <h3 className="text-2xl font-bold">Start the timer to make a {selectedEgg} egg!</h3>
 
 
@@ -36,11 +53,12 @@ function App() {
             document.querySelector("h3").innerText = `Let's go!`;
            
             //start timer and show progress
-            let timeLeft = eggBoilTime * 60;
+            let timeLeft = eggBoilTime;
             const timer = setInterval(() => {
               if (timeLeft <= 0) {
                 clearInterval(timer);
                 document.querySelector("h3").innerText = `Your ${selectedEgg} egg is ready!`;
+                setIsReady(true);
 
                 //remove bounce animation from image when egg is ready
                 if (img) {
