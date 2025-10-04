5d2ce306cf0be62cc09b537315fcda83470fc150
Zlatina Hristova <zlatina.nyagolova@gmail.com>
2025-10-04 17:51:27 +0300

    Deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+<<<<<<< HEAD
+  base: './'
+=======
+  base: './'
+>>>>>>> 28c042e5c6268c8de6c8527affc7cdd512d5649d
 })
